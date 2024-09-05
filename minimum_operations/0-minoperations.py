33273c0f1f2cddf933cbd4f303492b38a138847e
BYUUSAA <byuussaa@gmail.com>
2024-09-05 08:51:05 +0100

    0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -17,15 +17,15 @@ def minOperations(n):
     # Impossible cases: when n is 0 or negative
     if n <= 1:
         return 0
-    
+
     operations = 0
     divisor = 2
-    
+
     # Iteratively reduce n by its factors, counting operations
     while n > 1:
         while n % divisor == 0:
             operations += divisor
             n //= divisor
         divisor += 1
-    
+
     return operations
